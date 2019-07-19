509928f1e52591ba9569654191b61305f8f1b196
bstorozhuk <storozhuk.b.m@gmail.com>
2019-07-19 22:55:55 +0300

    Move commands into global space

--- make/internal/make.go
+++ make/internal/make.go
@@ -7,25 +7,24 @@ import (
 const CoverageName = `coverage.out`
 
 var b = NewBuild(BuildOptions{})
+var commands = []Command{
+	{`build`, b.RunCmd(Go, `build`, `./...`)},
+	{`buildInlineBounds`, b.RunCmd(
+		Go, `build`, `-gcflags='-m -d=ssa/check_bce/debug=1'`, `./...`,
+	)},
+
+	{`test`, b.RunCmd(Go, `test`, `./...`)},
+	{`testDebug`, b.RunCmd(Go, `test`, `-v`, `./...`)},
+
+	{`coverage`, func() {
+		clean()
+		b.Run(Go, `test`, `-coverpkg=./...`, `-coverprofile=`+CoverageName, `./lib/arena/...`)
+		b.Run(Go, `tool`, `cover`, `-html=`+CoverageName)
+	}},
+	{`clean`, clean},
+}
 
 func main() {
-	commands := []Command{
-		{`build`, b.RunCmd(Go, `build`, `./...`)},
-		{
-			`buildInlineBounds`,
-			b.RunCmd(Go, `build`, `-gcflags='-m -d=ssa/check_bce/debug=1'`, `./...`),
-		},
-
-		{`test`, b.RunCmd(Go, `test`, `./...`)},
-		{`testDebug`, b.RunCmd(Go, `test`, `-v`, `./...`)},
-
-		{`coverage`, func() {
-			clean()
-			b.Run(Go, `test`, `-coverpkg=./...`, `-coverprofile=`+CoverageName, `./lib/arena/...`)
-			b.Run(Go, `tool`, `cover`, `-html=`+CoverageName)
-		}},
-		{`clean`, clean},
-	}
 	b.Register(commands)
 	b.Build(os.Args[1:])
 }
